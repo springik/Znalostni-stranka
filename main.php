0d534c19f9aed084ef9bdd863d0ccdc2b0280882
Richard Florian <springik.games@email.cz>
2023-04-03 21:16:19 +0200

    base of the profile site

--- main.php
+++ main.php
@@ -3,6 +3,7 @@
     $_SESSION["uniqueCheck"] = null;
     $_SESSION["loginSuccess"] = null;
     $_SESSION["logged_in"] = null;
+    $_SESSION["email"] = null;
     header("Location: registration.php");
     die();
 ?>
--- registration.php
+++ registration.php
@@ -7,7 +7,7 @@
     <title>Registration</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-secondary">
 
     <div class="text-center mt-5 w-auto justify-content-center align-items-center d-flex"> <!-- class="translate-middle" z nějakého důvodu u mě nefunguje klasicky odstreli vse do stratosfery -->
         <form class="" action="registration-server.php" method="post">
